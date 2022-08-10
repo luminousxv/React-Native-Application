ea4e70aa3f7eb3186fa396aac395a74460deb3a2
luminousxv <drgvyhn@gmail.com>
2022-08-10 22:40:33 +0900

    added TimeTable Stack.Screen

--- TUK_BUS_Application/App.tsx
+++ TUK_BUS_Application/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TabScreen from './src/component/pages/TabScreen';
+import TimeTable from './src/component/UI/molecule/TimeTable';
 import {RootStackParamList} from './types/navigation/types';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
@@ -16,6 +17,13 @@ function App() {
             name="TabScreen"
             component={TabScreen}
             options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="TimeTable"
+            component={TimeTable}
+            options={({route}) => ({
+              title: route.params.status + ' 전체시간표',
+            })}
           />
         </Stack.Navigator>
       </NavigationContainer>
