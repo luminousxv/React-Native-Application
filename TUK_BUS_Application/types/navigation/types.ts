47413076e579be8f2ead62a70e780f3d51816f49
luminousxv <drgvyhn@gmail.com>
2022-08-10 22:38:33 +0900

    added TimeTable Stack.Screen navigation, route type definition

--- TUK_BUS_Application/types/navigation/types.ts
+++ TUK_BUS_Application/types/navigation/types.ts
@@ -2,6 +2,9 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 export type RootStackParamList = {
   TabScreen: undefined;
+  TimeTable: {
+    status: string;
+  };
 };
 
 export type TabParamList = {
@@ -10,9 +13,24 @@ export type TabParamList = {
 };
 
 type Tab_Props = NativeStackScreenProps<RootStackParamList, 'TabScreen'>;
+type TimeTable_Props = NativeStackScreenProps<RootStackParamList, 'TimeTable'>;
 
 type TabNavigationProp = Tab_Props['navigation'];
+export type TimeTableNavigationProp = TimeTable_Props['navigation'];
+
+export type TimeTableRouteProp = TimeTable_Props['route'];
 
 export type Tab_Screen_Props = {
   navigation: TabNavigationProp;
+};
+
+export type Time_Table_Props = {
+  navigation: TimeTableNavigationProp;
+  route: TimeTableRouteProp;
+};
+
+export type TimeInfo = {
+  time: string;
+  remain: number;
+  arrival: string;
 };
