6fe7eaa12c294e3228a1ccd569b860c830ff4786
luminousxv <drgvyhn@gmail.com>
2022-08-24 16:37:20 +0900

    added datatable for bus time table page

--- TUK_BUS_Application/src/component/UI/molecule/TimeTable.tsx
+++ TUK_BUS_Application/src/component/UI/molecule/TimeTable.tsx
@@ -1,20 +1,40 @@
 import React, {ReactElement} from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 import {Time_Table_Props} from '../../../../types/navigation/types';
+import {DataTable} from 'react-native-paper';
+import {styles} from '../atom/stylesheet';
 
 export default function TimeTable({route}: Time_Table_Props): ReactElement {
   const {status} = route.params;
 
   if (status === '등교') {
     return (
-      <View>
-        <Text> 등교 전체 시간표</Text>
+      <View style={styles.timetable_container}>
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title>시간</DataTable.Title>
+            <DataTable.Title>시간</DataTable.Title>
+          </DataTable.Header>
+          <DataTable.Row>
+            <DataTable.Cell>23:00</DataTable.Cell>
+            <DataTable.Cell>23:20</DataTable.Cell>
+          </DataTable.Row>
+        </DataTable>
       </View>
     );
   }
   return (
-    <View>
-      <Text> 하교 전체 시간표</Text>
+    <View style={styles.timetable_container}>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>시간</DataTable.Title>
+          <DataTable.Title>시간</DataTable.Title>
+        </DataTable.Header>
+        <DataTable.Row>
+          <DataTable.Cell>23:00</DataTable.Cell>
+          <DataTable.Cell>23:20</DataTable.Cell>
+        </DataTable.Row>
+      </DataTable>
     </View>
   );
 }
