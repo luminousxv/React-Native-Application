f9e299a6de4dc3b5cc2179256f1567abe13e695f
luminousxv <drgvyhn@gmail.com>
2022-08-27 20:23:13 +0900

    changed UI of Arrival Time Component changed fontSize, color, and placement of texts applied TouchOpacity component for the first component of FlatList

--- TUK_BUS_Application/src/component/UI/molecule/Go_University.tsx
+++ TUK_BUS_Application/src/component/UI/molecule/Go_University.tsx
@@ -1,8 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {ReactElement, useEffect, useState} from 'react';
-import {FlatList, RefreshControl, StatusBar, Text, View} from 'react-native';
+import {
+  FlatList,
+  RefreshControl,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {TimeInfo} from '../../../../types/navigation/types';
-import {styles} from '../atom/stylesheet';
+import {styles} from '../atom/stylesheet.css';
 import {CalcArrivalTime, CalcRemainTime} from '../atom/calctime';
 import {getArrivalTime} from '../../../api/arrivalTimeAPI';
 
@@ -18,6 +25,7 @@ export function GoUniversity(): ReactElement {
   const [refreshing, setRefreshing] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [univ_bustime, setUniv_Bustime] = useState<string[]>([]);
+  const [isVisible, setVisible] = useState<boolean>(false);
 
   const onRefresh = () => {
     setTimeInfo([]);
@@ -88,31 +96,69 @@ export function GoUniversity(): ReactElement {
               tintColor={'black'}
             />
           }
-          renderItem={({item}) => (
-            <>
+          renderItem={({item, index}) => {
+            if (index === 0) {
+              return (
+                <View>
+                  <TouchableOpacity
+                    style={styles.arrival_container}
+                    onPress={() => setVisible(!isVisible)}>
+                    <View style={styles.time_container}>
+                      <Text style={styles.time_text}>{item.time}</Text>
+                    </View>
+                    <View style={styles.infoText_container}>
+                      <View style={styles.calctime_container}>
+                        <Text style={styles.left_time_text}>
+                          {item.remain}분
+                        </Text>
+                        <Text style={styles.arrival_time_text}>
+                          {item.arrival}
+                        </Text>
+                      </View>
+                      <View style={styles.time_container}>
+                        <Text style={styles.remain_text}>남은시간</Text>
+                        <Text style={styles.arrival_text}>도착예정시간</Text>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                  <View>
+                    {isVisible && (
+                      <View style={styles.info_container}>
+                        <Text style={styles.sub_text}>
+                          지하철 실시간 위치 정보
+                        </Text>
+                        <View style={styles.sub_container}>
+                          <Text style={styles.sub_text}>당고개행: 10분전</Text>
+                          <Text style={styles.sub_text}>오이도행: 7분전</Text>
+                        </View>
+                        <View style={styles.sub_container}>
+                          <Text style={styles.sub_text}>왕십리행 : 5분전</Text>
+                          <Text style={styles.sub_text}> 인천행 : 20분전</Text>
+                        </View>
+                      </View>
+                    )}
+                  </View>
+                </View>
+              );
+            }
+            return (
               <View style={styles.arrival_container}>
-                <Text style={styles.text}>
-                  {item.time}
-                  {'\n'}
-                  남은시간: {item.remain}분{'\n'}
-                  도착예정시간: {item.arrival}
-                </Text>
-              </View>
-              <View style={styles.info_container}>
-                <Text style={styles.sub_text}>
-                  지하철 도착 정보(4호선, 수인분당선)
-                </Text>
-                <View style={styles.sub_container}>
-                  <Text style={styles.sub_text}>당고개행: 10분전</Text>
-                  <Text style={styles.sub_text}>오이도행: 7분전</Text>
+                <View style={styles.time_container}>
+                  <Text style={styles.time_text}>{item.time}</Text>
                 </View>
-                <View style={styles.sub_container}>
-                  <Text style={styles.sub_text}>왕십리행 : 5분전</Text>
-                  <Text style={styles.sub_text}> 인천행 : 20분전</Text>
+                <View style={styles.infoText_container}>
+                  <View style={styles.calctime_container}>
+                    <Text style={styles.left_time_text}>{item.remain}분</Text>
+                    <Text style={styles.arrival_time_text}>{item.arrival}</Text>
+                  </View>
+                  <View style={styles.time_container}>
+                    <Text style={styles.remain_text}>남은시간</Text>
+                    <Text style={styles.arrival_text}>도착예정시간</Text>
+                  </View>
                 </View>
               </View>
-            </>
-          )}
+            );
+          }}
         />
       </View>
     );
