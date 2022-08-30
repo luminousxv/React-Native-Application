016beac3ba502f76cdea182ac104cfea9d19c635
luminousxv <drgvyhn@gmail.com>
2022-08-30 19:17:11 +0900

    applied modularization of time info component and live subway location component

--- TUK_BUS_Application/src/component/UI/molecule/Go_Home.tsx
+++ TUK_BUS_Application/src/component/UI/molecule/Go_Home.tsx
@@ -12,6 +12,7 @@ import {TimeInfo} from '../../../../types/navigation/types';
 import {styles} from '../atom/stylesheet.css';
 import {CalcArrivalTime, CalcRemainTime} from '../atom/calctime';
 import {getArrivalTime} from '../../../api/arrivalTimeAPI';
+import {InfoContainer, SubwayContainer} from '../atom/info_subway_container';
 
 const wait = (timeout: number) => {
   return new Promise<void>(resolve => {
@@ -105,59 +106,15 @@ export function GoHome(): ReactElement {
                   <TouchableOpacity
                     style={styles.arrival_container}
                     onPress={() => setVisible(!isVisible)}>
-                    <View style={styles.time_container}>
-                      <Text style={styles.time_text}>{item.time}</Text>
-                    </View>
-                    <View style={styles.infoText_container}>
-                      <View style={styles.calctime_container}>
-                        <Text style={styles.left_time_text}>
-                          {item.remain}분
-                        </Text>
-                        <Text style={styles.arrival_time_text}>
-                          {item.arrival}
-                        </Text>
-                      </View>
-                      <View style={styles.time_container}>
-                        <Text style={styles.remain_text}>남은시간</Text>
-                        <Text style={styles.arrival_text}>도착예정시간</Text>
-                      </View>
-                    </View>
+                    <InfoContainer item={item} />
                   </TouchableOpacity>
-                  <View>
-                    {isVisible && (
-                      <View style={styles.info_container}>
-                        <Text style={styles.sub_text}>
-                          지하철 실시간 위치 정보
-                        </Text>
-                        <View style={styles.sub_container}>
-                          <Text style={styles.sub_text}>당고개행: 10분전</Text>
-                          <Text style={styles.sub_text}>오이도행: 7분전</Text>
-                        </View>
-                        <View style={styles.sub_container}>
-                          <Text style={styles.sub_text}>왕십리행 : 5분전</Text>
-                          <Text style={styles.sub_text}> 인천행 : 20분전</Text>
-                        </View>
-                      </View>
-                    )}
-                  </View>
+                  <View>{!isVisible && <SubwayContainer />}</View>
                 </View>
               );
             }
             return (
               <View style={styles.arrival_container}>
-                <View style={styles.time_container}>
-                  <Text style={styles.time_text}>{item.time}</Text>
-                </View>
-                <View style={styles.infoText_container}>
-                  <View style={styles.calctime_container}>
-                    <Text style={styles.left_time_text}>{item.remain}분</Text>
-                    <Text style={styles.arrival_time_text}>{item.arrival}</Text>
-                  </View>
-                  <View style={styles.time_container}>
-                    <Text style={styles.remain_text}>남은시간</Text>
-                    <Text style={styles.arrival_text}>도착예정시간</Text>
-                  </View>
-                </View>
+                <InfoContainer item={item} />
               </View>
             );
           }}
