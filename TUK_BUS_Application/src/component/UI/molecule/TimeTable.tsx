dcf0884b4710f5ecc3d1d9402f6d296073ffabe4
luminousxv <drgvyhn@gmail.com>
2022-09-02 17:15:21 +0900

    removed route parameter due to unnecessary variable

--- TUK_BUS_Application/src/component/UI/molecule/TimeTable.tsx
+++ TUK_BUS_Application/src/component/UI/molecule/TimeTable.tsx
@@ -1,28 +1,9 @@
 import React, {ReactElement} from 'react';
 import {View} from 'react-native';
-import {Time_Table_Props} from '../../../../types/navigation/types';
 import {DataTable} from 'react-native-paper';
 import {styles} from '../atom/stylesheet.css';
 
-export default function TimeTable({route}: Time_Table_Props): ReactElement {
-  const {status} = route.params;
-
-  if (status === '등교') {
-    return (
-      <View style={styles.timetable_container}>
-        <DataTable>
-          <DataTable.Header>
-            <DataTable.Title>시간</DataTable.Title>
-            <DataTable.Title>시간</DataTable.Title>
-          </DataTable.Header>
-          <DataTable.Row>
-            <DataTable.Cell>23:00</DataTable.Cell>
-            <DataTable.Cell>23:20</DataTable.Cell>
-          </DataTable.Row>
-        </DataTable>
-      </View>
-    );
-  }
+export default function TimeTable(): ReactElement {
   return (
     <View style={styles.timetable_container}>
       <DataTable>
