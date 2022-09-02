ea46c85708d7a78b104abce5be49b4c2745b1eb9
luminousxv <drgvyhn@gmail.com>
2022-09-02 17:11:51 +0900

    changed from TabScreen to DrawerScreen Nested TabScreen from DrawerScreen

--- TUK_BUS_Application/App.tsx
+++ TUK_BUS_Application/App.tsx
@@ -1,10 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import TabScreen from './src/component/pages/TabScreen';
 import TimeTable from './src/component/UI/molecule/TimeTable';
 import {RootStackParamList} from './types/navigation/types';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import DrawerScreen from './src/component/pages/DrawerScreen';
+// import TabScreen from './src/component/pages/TabScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -14,10 +16,15 @@ function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
+            name="DrawerScreen"
+            component={DrawerScreen}
+            options={{headerShown: false}}
+          />
+          {/* <Stack.Screen
             name="TabScreen"
             component={TabScreen}
             options={{headerShown: false}}
-          />
+          /> */}
           <Stack.Screen
             name="TimeTable"
             component={TimeTable}
