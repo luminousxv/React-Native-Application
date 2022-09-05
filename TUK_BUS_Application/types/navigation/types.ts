1fee5bd6bb86e40d5f349c20d452dba21bbdcaed
luminousxv <drgvyhn@gmail.com>
2022-09-05 19:50:24 +0900

    changed tab navigator's type definition

--- TUK_BUS_Application/types/navigation/types.ts
+++ TUK_BUS_Application/types/navigation/types.ts
@@ -1,34 +1,12 @@
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-
 export type RootStackParamList = {
-  DrawerScreen: undefined;
   TabScreen: undefined;
-  TimeTable: {
-    status: string;
-  };
-  ShuttleLocation: undefined;
 };
 
 export type TabParamList = {
   등교: undefined;
   하교: undefined;
   전체시간표: undefined;
-};
-
-export type DrawerParamList = {
-  홈: undefined;
   타는위치: undefined;
-};
-
-type TimeTable_Props = NativeStackScreenProps<RootStackParamList, 'TimeTable'>;
-
-export type TimeTableNavigationProp = TimeTable_Props['navigation'];
-
-export type TimeTableRouteProp = TimeTable_Props['route'];
-
-export type Time_Table_Props = {
-  navigation: TimeTableNavigationProp;
-  route: TimeTableRouteProp;
 };
 
 export type TimeInfo = {
