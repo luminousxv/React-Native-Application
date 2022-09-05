ac0b4a70dab3b704280ee127750f679fdcd04374
luminousxv <drgvyhn@gmail.com>
2022-09-05 19:49:15 +0900

    changed from DrawerScreen to TabScreen

--- TUK_BUS_Application/App.tsx
+++ TUK_BUS_Application/App.tsx
@@ -2,12 +2,9 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import TimeTable from './src/component/UI/molecule/TimeTable';
 import {RootStackParamList} from './types/navigation/types';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import DrawerScreen from './src/component/pages/DrawerScreen';
-// import TabScreen from './src/component/pages/TabScreen';
-
+import TabScreen from './src/component/pages/TabScreen';
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
@@ -16,21 +13,9 @@ function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
-            name="DrawerScreen"
-            component={DrawerScreen}
-            options={{headerShown: false}}
-          />
-          {/* <Stack.Screen
             name="TabScreen"
             component={TabScreen}
             options={{headerShown: false}}
-          /> */}
-          <Stack.Screen
-            name="TimeTable"
-            component={TimeTable}
-            options={({route}) => ({
-              title: route.params.status + ' 전체시간표',
-            })}
           />
         </Stack.Navigator>
       </NavigationContainer>
