7c7027017d159fa78291b5c173482faf4f04cf34
luminousxv <drgvyhn@gmail.com>
2022-09-05 19:51:21 +0900

    changed the ui of the subway live location info container

--- TUK_BUS_Application/src/component/UI/atom/info_subway_container.tsx
+++ TUK_BUS_Application/src/component/UI/atom/info_subway_container.tsx
@@ -26,14 +26,22 @@ export function InfoContainer({item}: flatlistParams): ReactElement {
 export function SubwayContainer(): ReactElement {
   return (
     <View style={styles.info_container}>
-      <Text style={styles.sub_text}>지하철 실시간 위치 정보</Text>
-      <View style={styles.sub_container}>
-        <Text style={styles.sub_text}>당고개행: 10분전</Text>
-        <Text style={styles.sub_text}>오이도행: 7분전</Text>
-      </View>
-      <View style={styles.sub_container}>
-        <Text style={styles.sub_text}>왕십리행 : 5분전</Text>
-        <Text style={styles.sub_text}> 인천행 : 20분전</Text>
+      <Text style={styles.sub_title_text}>
+        지하철 실시간 위치 정보 (정왕역)
+      </Text>
+      <View style={{flexDirection: 'row'}}>
+        <View style={styles.sub_container}>
+          <Text style={styles.sub_direction_text}>당고개행</Text>
+          <Text style={styles.sub_direction_text}>오이도행</Text>
+          <Text style={styles.sub_direction_text}>왕십리행</Text>
+          <Text style={styles.sub_direction_text}>인천행</Text>
+        </View>
+        <View style={styles.sub_container}>
+          <Text style={styles.sub_location_text}>정보없음</Text>
+          <Text style={styles.sub_location_text}>안산(전전역)</Text>
+          <Text style={styles.sub_location_text}>사리포구(4전역)</Text>
+          <Text style={styles.sub_location_text}>중앙(5전역)</Text>
+        </View>
       </View>
     </View>
   );
