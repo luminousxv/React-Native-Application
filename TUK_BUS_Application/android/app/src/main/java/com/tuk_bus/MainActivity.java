c615d1189158671878071933f95669d6ad80d945
luminousxv <drgvyhn@gmail.com>
2022-09-24 15:33:57 +0900

    android build bundle update

--- TUK_BUS_Application/android/app/src/main/java/com/tuk_bus/MainActivity.java
+++ TUK_BUS_Application/android/app/src/main/java/com/tuk_bus/MainActivity.java
@@ -3,13 +3,39 @@ package com.tuk_bus;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
 
 public class MainActivity extends ReactActivity {
+
+  private void getAppKeyHash() {
+    try {
+      PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+      for (Signature signature : info.signatures) {
+        MessageDigest md;
+        md = MessageDigest.getInstance("SHA");
+        md.update(signature.toByteArray());
+        String something = new String(Base64.encode(md.digest(), 0));
+        Log.e("Hash key", something);
+      }
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      Log.e("name not found", e.toString());
+    }
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
+
+    getAppKeyHash();
   }
 
   /**
