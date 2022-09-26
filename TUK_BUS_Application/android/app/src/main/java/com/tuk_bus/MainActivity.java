1ed40b4f610cf3beed4118fcd109da03933980d8
luminousxv <drgvyhn@gmail.com>
2022-09-26 09:34:14 +0900

    updated android bundle

--- TUK_BUS_Application/android/app/src/main/java/com/tuk_bus/MainActivity.java
+++ TUK_BUS_Application/android/app/src/main/java/com/tuk_bus/MainActivity.java
@@ -15,27 +15,9 @@ import java.security.MessageDigest;
 
 public class MainActivity extends ReactActivity {
 
-  private void getAppKeyHash() {
-    try {
-      PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-      for (Signature signature : info.signatures) {
-        MessageDigest md;
-        md = MessageDigest.getInstance("SHA");
-        md.update(signature.toByteArray());
-        String something = new String(Base64.encode(md.digest(), 0));
-        Log.e("Hash key", something);
-      }
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      Log.e("name not found", e.toString());
-    }
-  }
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
-
-    getAppKeyHash();
   }
 
   /**
